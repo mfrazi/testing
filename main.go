994a1d16d8fe498054c7868930b8bff2b430c957
mfrazi <fahrul.razi@tokopedia.com>
2020-09-30 16:23:24 +0700

    lubang 4

--- main.go
+++ main.go
@@ -5,5 +5,6 @@ import "log"
 func main() {
 	log.Println("rafael suka gali lubang")
 	log.Println("rafael suka gali lubang 2")
+	log.Println("rafael suka gali lubang 4")
 	log.Println("rafael suka gali lubang 3")
 }
